dc40f60bd870924e5a4b2e9232893f44b5155875
Muhammad Mishbahul muflihin <mishbahulmuflihin@gmail.com>
2026-03-18 16:01:04 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6468065292-dc3aa",
+  "appId": "1:647694388184:web:da001ad7f5b9df998680ad",
+  "apiKey": "AIzaSyDY1askpWIaD9jssdPp4zC7lu8Syz8UOB4",
+  "authDomain": "studio-6468065292-dc3aa.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "647694388184"
+};
